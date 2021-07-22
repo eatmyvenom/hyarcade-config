634a66eeb4594160f858dd43a778b36307f9fb26
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-22 11:18:46 -0700

    add runtime to config

---
+++ Runtime.js
@@ -0,0 +1,18 @@
+const ffs = require("fs");
+const logger = require("hyarcade-logger");
+
+module.exports = class Runtime {
+    async save() {
+        logger.debug("Saving runtime info json");
+        await ffs.promises.writeFile("runtimeinfo.json", JSON.stringify(this, null, 2));
+    }
+
+    static fromJSON() {
+        let newRun = new Runtime();
+        let json = JSON.parse(ffs.readFileSync("data/runtimeinfo.json"));
+        for (let p in json) {
+            newRun[p] = json[p];
+        }
+        return newRun;
+    }
+};
