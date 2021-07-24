b3497b4bec1e1f5305f4c453235bc0cf6ab77dee
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-23 22:14:04 -0700

    allow for multiple config files

--- Config.js
+++ Config.js
@@ -1,9 +1,90 @@
-const fs = require("fs/promises");
-const ffs = require("fs");
+const fs = require("fs");
+const { Snowflake, Presence } = require("discord.js")
+
+class BotUser {
+    username = "";
+    icon = "";
+    /**
+     *
+     * @type {Presence[]}
+     * @memberof BotUser
+     */
+    presences = [];
+}
+
+class DiscordConfig {
+    token = "";
+    backupToken = "";
+    miniToken = "";
+    mwToken = "";
+    testToken = "";
+    
+    /**
+     *
+     * @type {String[]}
+     * @memberof DiscordConfig
+     */
+    listenChannels = [];
+
+    /**
+     *
+     * @type {String[]}
+     * @memberof DiscordConfig
+     */
+    trustedUsers = [];
+
+    /**
+     *
+     * @type {Snowflake}
+     * @memberof DiscordConfig
+     */
+    logChannel = "";
+
+    /**
+     *
+     * @type {Snowflake}
+     * @memberof DiscordConfig
+     */
+    errChannel = "";
+
+    /**
+     *
+     * @type {Presence[]}
+     * @memberof DiscordConfig
+     */
+    presences = [];
+
+    /**
+     *
+     * @type {Object.<string, BotUser>}
+     * @memberof DiscordConfig
+     */
+    setup = {};
+}
+
+class Webhook {
+    id = "";
+    token = "";
+    username = "";
+    pfp = "";
+}
+
+class EventManager {
+    /**
+     *
+     * @type {Webhook}
+     * @memberof EventManager
+     */
+    webhook = {};
+    winMod = 0;
+    name = "";
+}
 
 class Config {
     key = "";
     altkeys = [];
+    dbURL = "";
+    dbPass = "";
     mode = "";
     alwaysForce = false;
     logRateLimit = true;
@@ -11,15 +92,51 @@ class Config {
     cluster = "";
     sortDirection = "";
     printAllWins = false;
+    arcadeWinLimit = 0;
+    cringeGameLowerBound = 0;
+    cringeGameUpperBound = 0;
     showDaytime = false;
-    clusterTarget = "";
     commandCharacter = "";
+    clusterTarget = "";
     clusters = {};
-    discord = {};
+
+    /**
+     *
+     * @type {Object.<string, EventManager>}
+     * @memberof Config
+     */
     events = {};
+
+    /**
+     *
+     * @type {Webhook}
+     * @memberof Config
+     */
     webhook = {};
-    mojang = {};
-    std = {};
+
+    /**
+     *
+     * @type {DiscordConfig}
+     * @memberof Config
+     */
+    discord = {};
+
+    /**
+     *
+     * @type {Object.<string, Webhook>}
+     * @memberof Config
+     */
+    otherHooks = {};
+
+    mojang = class MojangSettings{
+        sleep = 0;
+    };
+
+    std = class STDControl{
+        disable = false;
+        out = "";
+        err = "";
+    };
 
     constructor(json) {
         for (let thing in json) {
@@ -28,16 +145,12 @@ class Config {
     }
 
     static fromJSON() {
-        return new Config(JSON.parse(ffs.readFileSync("./config.json")));
+        let cfg = new Config(JSON.parse(fs.readFileSync("config.json")));
+        cfg.events = JSON.parse(fs.readFileSync("discord.events.json"));
+        cfg.discord = JSON.parse(fs.readFileSync("discord.json"));
+        cfg.otherHooks = JSON.parse(fs.readFileSync("discord.timed.json"));
     }
 
-    static fromEnv() {
-        return new Config(JSON.parse(process.env.configjson));
-    }
-
-    async writeConfig() {
-        await fs.writeFile("./config.json", JSON.stringify(this, null, 4));
-    }
 }
 
 module.exports = Config;
