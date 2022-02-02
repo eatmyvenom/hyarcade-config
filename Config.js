b78f6acc86516cb25f7a812eb175effa9580b197
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-01 19:50:52 -0800

    style(lint): :sparkles: update repo tools
    Updates most tools used in the repo.
    NPM -> yarn
    eslint -> eslint+prettier
    -> husky
    -> commitlint
    
    NPM is no longer used

--- Config.js
+++ Config.js
@@ -1,170 +1,166 @@
 const fs = require("fs");
-const {
-  Snowflake,
-  Presence
-} = require("discord.js");
+const { Snowflake, Presence } = require("discord.js");
 
 class BotUser {
-    username = "";
-    icon = "";
-    /**
-     *
-     * @type {Presence[]}
-     * @memberof BotUser
-     */
-    presences = [];
+  username = "";
+  icon = "";
+  /**
+   *
+   * @type {Presence[]}
+   * @memberof BotUser
+   */
+  presences = [];
 }
 
 class DiscordConfig {
-    token = "";
-    backupToken = "";
-    miniToken = "";
-    mwToken = "";
-    testToken = "";
+  token = "";
+  backupToken = "";
+  miniToken = "";
+  mwToken = "";
+  testToken = "";
 
-    /**
-     *
-     * @type {string[]}
-     * @memberof DiscordConfig
-     */
-    listenChannels = [];
+  /**
+   *
+   * @type {string[]}
+   * @memberof DiscordConfig
+   */
+  listenChannels = [];
 
-    /**
-     *
-     * @type {string[]}
-     * @memberof DiscordConfig
-     */
-    trustedUsers = [];
+  /**
+   *
+   * @type {string[]}
+   * @memberof DiscordConfig
+   */
+  trustedUsers = [];
 
-    /**
-     *
-     * @type {Snowflake}
-     * @memberof DiscordConfig
-     */
-    logChannel = "";
+  /**
+   *
+   * @type {Snowflake}
+   * @memberof DiscordConfig
+   */
+  logChannel = "";
 
-    /**
-     *
-     * @type {Snowflake}
-     * @memberof DiscordConfig
-     */
-    errChannel = "";
+  /**
+   *
+   * @type {Snowflake}
+   * @memberof DiscordConfig
+   */
+  errChannel = "";
 
-    /**
-     *
-     * @type {Presence[]}
-     * @memberof DiscordConfig
-     */
-    presences = [];
+  /**
+   *
+   * @type {Presence[]}
+   * @memberof DiscordConfig
+   */
+  presences = [];
 
-    /**
-     *
-     * @type {object.<string, BotUser>}
-     * @memberof DiscordConfig
-     */
-    setup = {};
+  /**
+   *
+   * @type {object.<string, BotUser>}
+   * @memberof DiscordConfig
+   */
+  setup = {};
 }
 
 class Webhook {
-    id = "";
-    token = "";
-    username = "";
-    pfp = "";
+  id = "";
+  token = "";
+  username = "";
+  pfp = "";
 }
 
 class EventManager {
-    /**
-     *
-     * @type {Webhook}
-     * @memberof EventManager
-     */
-    webhook = {};
-    winMod = 0;
-    name = "";
+  /**
+   *
+   * @type {Webhook}
+   * @memberof EventManager
+   */
+  webhook = {};
+  winMod = 0;
+  name = "";
 }
 
 class Config {
-    key = "";
-    altkeys = [];
-    dbURL = "";
-    dbPass = "";
-    mode = "";
-    alwaysForce = false;
-    logRateLimit = true;
-    watchdogTimeout = 30000;
-    cluster = "";
-    sortDirection = "";
-    printAllWins = false;
-    arcadeWinLimit = 0;
-    cringeGameLowerBound = 0;
-    cringeGameUpperBound = 0;
-    showDaytime = false;
-    commandCharacter = "";
-    clusterTarget = "";
-    clusters = {};
+  key = "";
+  altkeys = [];
+  dbURL = "";
+  dbPass = "";
+  mode = "";
+  alwaysForce = false;
+  logRateLimit = true;
+  watchdogTimeout = 30000;
+  cluster = "";
+  sortDirection = "";
+  printAllWins = false;
+  arcadeWinLimit = 0;
+  cringeGameLowerBound = 0;
+  cringeGameUpperBound = 0;
+  showDaytime = false;
+  commandCharacter = "";
+  clusterTarget = "";
+  clusters = {};
 
-    /**
-     *
-     * @type {object.<string, EventManager>}
-     * @memberof Config
-     */
-    events = {};
+  /**
+   *
+   * @type {object.<string, EventManager>}
+   * @memberof Config
+   */
+  events = {};
 
-    /**
-     *
-     * @type {Webhook}
-     * @memberof Config
-     */
-    webhook = {};
+  /**
+   *
+   * @type {Webhook}
+   * @memberof Config
+   */
+  webhook = {};
 
-    /**
-     *
-     * @type {DiscordConfig}
-     * @memberof Config
-     */
-    discord = {};
+  /**
+   *
+   * @type {DiscordConfig}
+   * @memberof Config
+   */
+  discord = {};
 
-    /**
-     *
-     * @type {object.<string, Webhook>}
-     * @memberof Config
-     */
-    otherHooks = {};
+  /**
+   *
+   * @type {object.<string, Webhook>}
+   * @memberof Config
+   */
+  otherHooks = {};
 
-    mojang = class MojangSettings {
-        sleep = 0;
-    };
+  mojang = class MojangSettings {
+    sleep = 0;
+  };
 
-    std = class STDControl {
-        disable = false;
-        out = "";
-        err = "";
-    };
+  std = class STDControl {
+    disable = false;
+    out = "";
+    err = "";
+  };
 
-    constructor (json) {
-      for(const thing in json) {
-        this[thing] = json[thing];
+  constructor(json) {
+    for (const thing in json) {
+      this[thing] = json[thing];
+    }
+  }
+
+  /**
+   *
+   * @returns {Config}
+   */
+  static fromJSON() {
+    const configs = fs.readdirSync("config");
+
+    const cfg = new Config(JSON.parse(fs.readFileSync("config/config.json")));
+
+    for (const file of configs) {
+      if (file.slice(-5, file.length) == ".json") {
+        cfg[file.replace(/\.json/g, "")] = JSON.parse(fs.readFileSync(`config/${file}`));
       }
     }
 
-    /**
-     * 
-     * @returns {Config}
-     */
-    static fromJSON () {
-      const configs = fs.readdirSync("config");
-
-      const cfg = new Config(JSON.parse(fs.readFileSync("config/config.json")));
-
-      for(const file of configs) {
-        if(file.slice(-5, file.length) == ".json") {
-          cfg[file.replace(/\.json/g, "")] = JSON.parse(fs.readFileSync(`config/${file}`));
-        }
-      }
-
-      return cfg;
-    }
-
+    return cfg;
+  }
 }
 
 module.exports = Config;
--- Runtime.js
+++ Runtime.js
@@ -2,17 +2,17 @@ const ffs = require("fs-extra");
 const logger = require("hyarcade-logger");
 
 module.exports = class Runtime {
-  async save () {
+  async save() {
     logger.debug("Saving runtime info json");
     await ffs.writeFile("data/runtimeinfo.json", JSON.stringify(this, null, 2));
   }
 
-  static fromJSON () {
+  static fromJSON() {
     const newRun = new Runtime();
     let json;
     try {
       json = JSON.parse(ffs.readFileSync("data/runtimeinfo.json"));
-      for(const p in json) {
+      for (const p in json) {
         newRun[p] = json[p];
       }
     } catch (e) {
